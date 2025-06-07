b7783a9765b852d20d2f0b49c93d446a675ea27c
ishanbagra <159798261+ishanbagra18@users.noreply.github.com>
2025-06-07 18:58:06 +0530

    Update server.js

--- backend/SocketIO/server.js
+++ backend/SocketIO/server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "mychatapp-nine.vercel.app",
+    origin: "https://mychatapp-m86k.onrender.com",
     methods: ["GET", "POST"],
   },
 });
