3e62b112c0f0204ab1e6238339ccef43b775fa19
ishanbagra <159798261+ishanbagra18@users.noreply.github.com>
2025-06-07 18:47:00 +0530

    Update server.js

--- backend/SocketIO/server.js
+++ backend/SocketIO/server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://mychatapp-t4g4.vercel.app/login",
+    origin: "mychatapp-nine.vercel.app",
     methods: ["GET", "POST"],
   },
 });
